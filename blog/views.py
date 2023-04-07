7de7c5e5be93f4fbf576a2c68410d26ba9d6e663
stefano <stephanedavy@outlook.fr>
2023-04-07 21:43:29 +0200

    amelioration du calendrier

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import datetime
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from .models import Article
@@ -18,7 +19,10 @@ def article_single(request, article):
   related = Article.objects.filter(auteur=article.auteur)[:5]
   return render(request, 'pages/article_single.html', {'article': article, 'related': related})
 
-def sticky_note(request):
+def sticky_note(request, year, month):
     current_date = datetime.datetime.now()
     print(current_date)  # ajouter cette ligne pour vérifier que la date est récupérée
-    return render(request, 'blog/components/sticky_note.html', {'current_date': current_date})
+
+    return render(request, 'blog/components/sticky_note.html', {
+      'current_date': current_date,
+    })
